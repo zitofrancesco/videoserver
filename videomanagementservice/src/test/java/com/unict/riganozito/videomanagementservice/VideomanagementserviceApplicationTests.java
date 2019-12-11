b2e0d9b3af9e89151932199c07511f3fdfeed76e
fabiorigano <fabio2rigano@gmail.com>
2019-12-11 12:44:55 +0100

    Initial commit for Video Server application (Distributed Systems project)

---
+++ videomanagementservice/src/test/java/com/unict/riganozito/videomanagementservice/VideomanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unict.riganozito.videomanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideomanagementserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ videoprocessingservice/src/test/java/com/unict/riganozito/videoprocessingservice/VideoprocessingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unict.riganozito.videoprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoprocessingserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
