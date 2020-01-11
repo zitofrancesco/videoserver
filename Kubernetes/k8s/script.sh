9d2a4916b225a0fcc03ef005554bfa73fcf0ca5c
fabiorigano <fabio2rigano@gmail.com>
2020-01-11 18:08:03 +0100

    Fixed k8s files: mysql, configmaps and APIGateway

--- Kubernetes/k8s/script.sh
+++ Kubernetes/k8s/script.sh
@@ -2,5 +2,5 @@ kubectl create configmap videoservice-config --from-file=./configmap
 kubectl apply -f ./kafka.yml
 kubectl apply -f ./mysql.yml
 
-
-kubectl apply -f ./test/apigatawey.yml
+cd ./test
+kubectl apply -f ./apigateway.yml
