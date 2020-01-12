845708e18c2cedc69acc427fe479b6dfc3e49f69
fabiorigano <fabio2rigano@gmail.com>
2020-01-12 10:01:00 +0100

    Created folders for k8s files

--- Kubernetes/k8s/script.sh
+++ Kubernetes/k8s/script.sh
@@ -1,6 +1,14 @@
-kubectl create configmap videoservice-config --from-file=./configmap
-kubectl apply -f ./kafka.yml
-kubectl apply -f ./mysql.yml
+kubectl create configmap videoservice-kafka --from-file=./kakfa/kafka.properties --save-config
+kubectl get configmap videoservice-kafka -o yaml > kakfa/videoservice-kafka.yml
+kubectl create configmap videoservice-mysql --from-file=./mysql/mysql.properties --save-config
+kubectl get configmap videoservice-mysql -o yaml > mysql/videoservice-mysql.yml
+kubectl create configmap videoservice-apigateway --from-file=./apigateway/apigateway.properties --save-config
+kubectl get configmap videoservice-apigateway -o yaml > apigateway/videoservice-apigateway.yml
+kubectl create configmap videoservice-videomanagementservice --from-file=./videomanagement/videomanagement.properties --save-config
+kubectl get configmap videoservice-videomanagementservice -o yaml > videomanagement/videoservice-videomanagement.yml
 
-cd ./test
-kubectl apply -f ./apigateway.yml
+## kubectl apply -f ./kafka.yml
+
+## kubectl apply -f ./mysql.yml
+
+## kubectl apply -f ./apigateway.yml
